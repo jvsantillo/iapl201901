d3ae94f1c0d67d952fa09b50b04893116adb5321
Márcio Flores <marciogabriel07@gmail.com>
2019-06-24 20:23:43 -0300

    Alteração no caminho da view signup

--- prestadores/views.py
+++ prestadores/views.py
@@ -11,7 +11,7 @@ from django.views import generic
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'signup.html'
+    template_name = 'prestadores/signup.html'
 
 from .models import Person
 
