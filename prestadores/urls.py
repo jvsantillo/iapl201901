47a221e1300c7edffc35ec0a5f7a7a583da728ce
João Vitor Rodrigues Santillo <jvsantillo@gmail.com>
2019-06-30 19:25:36 -0300

    Updating urls

--- prestadores/urls.py
+++ prestadores/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     #ex: /prestadores/
     path('', views.index, name='index'),
     #ex: /prestadores/create_person
-    path('api/create_person/$', views.create_person, name='create_person'),
+    path('api/create_person/', views.create_person, name='create_person'),
     #ex: /prestadores/person/1
     path('person/<int:person_id>/', views.person, name='person'),
     #ex: /prestadores/update_person/1
@@ -19,7 +19,7 @@ urlpatterns = [
     
 
     #ex: /prestadores/create_supplier
-    path('api/create_supplier/$', views.create_supplier, name='create_supplier'),
+    path('api/create_supplier/', views.create_supplier, name='create_supplier'),
     #ex: /prestadores/supplier/1
     path('supplier/<int:person_id>/', views.supplier, name='supplier'),
     #ex: /prestadores/update_supplier/1
@@ -30,7 +30,7 @@ urlpatterns = [
     path('retrieve_suppliers/', views.retrieve_suppliers, name='retrieve_suppliers'),
 
     #ex: /prestadores/create_expertise
-    path('api/create_expertise/$', views.create_expertise, name='create_expertise'),
+    path('api/create_expertise/', views.create_expertise, name='create_expertise'),
     #ex: /prestadores/expertise/1
     path('expertise/<int:person_id>/', views.expertise, name='expertise'),
     #ex: /prestadores/update_expertise/1
